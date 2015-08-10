9a047d77bb156c58b67f4103d3ae15bd006d8d81
cfaivre <christophe@faivre.co.za>
2015-08-10 15:57:56 +0200

    Add status to item field in populate script and expand on item specs

--- scripts/populate_items.rb
+++ scripts/populate_items.rb
@@ -11,31 +11,31 @@ end
 Item.delete_all
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: '45gergreg55576',
               rfid: '2015052900000000000000000000ABE2', purchase_order_number: 'PO111111',
-              sap_number: 'xxxxxxxxxxx'  )
+              sap_number: 'xxxxxxxxxxx', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: '45gergreg55577',
               rfid: '2015052900000000000000000000ABD1', purchase_order_number: 'PO111111',
-              sap_number: 'xxxxxxxxxxx'  )
+              sap_number: 'xxxxxxxxxxx', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: '45gergreg55578',
               rfid: '20150529000000008FF92F2500000000', purchase_order_number: 'PO111111',
-              sap_number: 'xxxxxxxxxxx'  )
+              sap_number: 'xxxxxxxxxxx', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: '45gergreg55579',
               rfid: '2015052900000000000000000000ABCF', purchase_order_number: 'PO111111',
-              sap_number: 'xxxxxxxxxxx'  )
+              sap_number: 'xxxxxxxxxxx', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: '45gergreg55580',
               rfid: '2015052900000000000000000000ABD0', purchase_order_number: 'PO111111',
-              sap_number: 'xxxxxxxxxxx'  )
+              sap_number: 'xxxxxxxxxxx', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: '45gergreg55581',
               rfid: '2015052900000000000000000000ABD4', purchase_order_number: 'PO111111',
-              sap_number: 'xxxxxxxxxxx'  )
+              sap_number: 'xxxxxxxxxxx', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: '45555555555582',
               rfid: '2015052900000000000000000000ABD2', purchase_order_number: 'PO111111',
-              sap_number: 'xxxxxxxxxxx'  )
+              sap_number: 'xxxxxxxxxxx', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: '45gergreg55583',
               rfid: '2015052900000000000000000000ABD5', purchase_order_number: 'PO111111',
-              sap_number: 'xxxxxxxxxxx'  )
+              sap_number: 'xxxxxxxxxxx', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: 'diosao87dsa784',
               rfid: '2015052900000000000000000000ABD3', purchase_order_number: 'PO890879',
-              sap_number: '32r3454tgre4'  )
+              sap_number: '32r3454tgre4', status: 'initialized' )
 Item.create!( location: 'supplier', date: Date.parse('2015-01-01'), serial_number: 'diosao84gfvv85',
               rfid: '2015052900000000000000000000ABD6', purchase_order_number: 'PO890879',
-              sap_number: '32r3454tgre4'  )
+              sap_number: '32r3454tgre4', status: 'initialized' )
